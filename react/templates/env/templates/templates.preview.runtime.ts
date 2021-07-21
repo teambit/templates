fbc170033459a32669a6b83233609640e00b9685
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2021-07-21 18:06:32 +0200

    update theme

--- react/templates/env/templates/templates.preview.runtime.ts
+++ react/templates/env/templates/templates.preview.runtime.ts
@@ -1,7 +1,7 @@
 import { PreviewRuntime } from '@teambit/preview';
 import { ReactAspect, ReactPreview } from '@teambit/react';
 import { TemplatesAspect } from './templates.aspect';
-import { ThemeCompositions } from '@teambit/documenter.theme.theme-compositions';
+import { ThemeContext } from '@teambit/design.theme.theme-context';
 
 export class TemplatesPreviewMain {
 
@@ -12,7 +12,7 @@ export class TemplatesPreviewMain {
   static async provider([react]: [ReactPreview]) {
     const templatesPreviewMain = new TemplatesPreviewMain();
     // register a new provider to wrap all compositions in the react environment with a custom theme.
-    react.registerProvider([ThemeCompositions]);
+    react.registerProvider([ThemeContext]);
 
     return templatesPreviewMain;
   }
