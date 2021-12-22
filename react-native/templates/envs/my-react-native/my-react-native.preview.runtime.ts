9d1d1e4146a735853fcae44f80eda784a6f13781
Josh Kuttler <yoshuakuttler@gmail.com>
2021-12-22 13:34:41 +0200

    update react-native preview template

--- react-native/templates/envs/my-react-native/my-react-native.preview.runtime.ts
+++ react-native/templates/envs/my-react-native/my-react-native.preview.runtime.ts
@@ -1,19 +1,18 @@
 import { PreviewRuntime } from '@teambit/preview';
-import { ReactAspect, ReactPreview } from '@teambit/react';
+import { ReactNativeAspect, ReactNativePreview } from '@teambit/react-native';
 // create your theme and import it here
 // import { ThemeCompositions } from '@my-company/my-scope.theme.theme-compositions';
-
 import { MyReactNativeAspect } from './my-react-native.aspect';
 
 export class MyReactNativePreviewMain {
   static runtime = PreviewRuntime;
 
-  static dependencies = [ReactAspect];
+  static dependencies = [ReactNativeAspect];
 
-  static async provider([react]: [ReactPreview]) {
+  static async provider([reactNative]: [ReactNativePreview]) {
     const myReactNativePreviewMain = new MyReactNativePreviewMain();
     // uncomment the line below to register a new provider to wrap all compositions using this environment with a custom theme.
-    // react.registerProvider([ThemeCompositions]);
+    // reactNative.registerProvider([ThemeCompositions]);
 
     return myReactNativePreviewMain;
   }
